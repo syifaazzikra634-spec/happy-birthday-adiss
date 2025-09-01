af6715c7b2d05ef9fb137399799d6bd8eab0dae0
syifaazzikra634-spec <syifaazzikra634@gmail.com>
2025-09-01 23:47:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -151,16 +151,5 @@ import streamlit as st
 </html>
 
 
-st.set_page_config(page_title="🎂 Birthday Greeting App", page_icon="🎈", layout="centered")
 
-st.title("🎉 Selamat Ulang Tahun 🎉")
-st.subheader("Adistya Salma Putri")
-st.write("Semoga sehat selalu, panjang umur, dan penuh kebahagiaan! 🥳")
-
-# 3 foto contoh (nanti bisa diganti dengan foto asli kamu)
-st.image("https://picsum.photos/400/200", caption="Foto 1")
-st.image("https://picsum.photos/400/200", caption="Foto 2")
-st.image("https://picsum.photos/400/200", caption="Foto 3")
-
-st.success("🎁 Dari sahabatmu dengan penuh doa terbaik 💕")
 
